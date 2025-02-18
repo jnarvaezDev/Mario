6073bbb5908812484f25e11040c24129cd94db7d
Jose Narvaez <jnarvaez@asylummarketing.com>
2025-02-18 14:34:06 -0500

    first commit

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 <head>
     <title>HTML5 games workshop: One-screen platformer</title>
     <meta charset="utf-8">
-<script
-  src="https://code.jquery.com/jquery-3.7.0.min.js"
-  integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
-  crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="js/phaser.min.js"></script>
-    <script src="js/main_completo.js?<?=md5(time())?>"></script>
+    <script src="js/main_completo.js?<?= md5(time()) ?>"></script>
 
     <style>
         canvas {
@@ -16,6 +14,8 @@
     </style>
 </head>
 <body>
-    <a href="#" class ="btn-game" >Iniciar</a>
-    <div id="game" ><img src="./images/nuevos/background.png" style="width:960px;height:770px;margin: 0 auto;display:block;"></div>
+    <a href="#" class="btn-game">Iniciar</a>
+    <div id="game"><img src="./images/nuevos/background.png"
+            style="width:960px;height:770px;margin: 0 auto;display:block;"></div>
 </body>
+</html>
